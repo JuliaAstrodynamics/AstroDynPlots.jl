052e4b6b5fa8d653d4b1a17718526aac542e8ec3
Helge Eichhorn <git@helgeeichhorn.de>
2019-08-29 20:02:53 +0200

    Use AstroBase

--- src/AstroDynPlots.jl
+++ src/AstroDynPlots.jl
@@ -1,7 +1,6 @@
 module AstroDynPlots
 
-using AstroDynBase
-using AstroDynCoordinates
+using AstroBase
 using RecipesBase
 using SmoothingSplines
 
@@ -26,14 +25,14 @@ function plot(tra::Trajectory)
     re = equatorial_radius(center)
     rp = polar_radius(center)
     n = 100
-    θ = linspace(-π/2, π/2, n)
-    ϕ = linspace(0, 2π, n)
+    θ = range(-π/2, stop=π/2, length=n)
+    ϕ = range(0, stop=2π, length=n)
     x = [re * cos(i) * cos(j) for i in θ, j in ϕ]
     y = [re * cos(i) * sin(j) for i in θ, j in ϕ]
     z = [rp * sin(i) for i in θ, j in ϕ];
     s = surface(x=x, y=y, z=z, colorscale="Blues", showlegend=false)
-    times = linspace(tra.times[1], tra.times[end], length(tra.times)*100)
-    p = scatter3d(;x=predict.(tra.splines[1], times), y=predict.(tra.splines[2], times), z=predict.(tra.splines[3], times), mode="lines", line=attr(color="rgb(255,0,0)"))
+    times = range(tra.times[1], stop=tra.times[end], length=length(tra.times)*100)
+    p = scatter3d(;x=predict.(Ref(tra.splines[1]), times), y=predict.(Ref(tra.splines[2]), times), z=predict.(Ref(tra.splines[3]), times), mode="lines", line=attr(color="rgb(255,0,0)"))
     plot([s, p])
 end
 
